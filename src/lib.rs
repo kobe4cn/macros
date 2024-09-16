2eac445ae4e4c91bebe4564969c843f25a651c3f
kobe4cn <kevin.yang.xgz@gmail.com>
2024-09-16 22:14:55 +0800

    feature: support generics for enum

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,11 @@ use quote::quote;
 #[proc_macro_derive(EnumFrom)]
 pub fn derive_enum_from(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
-
+    //get indent of the enum
     let ident = input.ident;
+    //get generic parameters of the enum
+    let generics = input.generics;
+    //get variants of the enum
     let variants = match input.data {
         syn::Data::Enum(data) => data.variants,
         _ => {
@@ -27,7 +30,7 @@ pub fn derive_enum_from(input: TokenStream) -> TokenStream {
                         .expect("Expected exactly one field in the variant");
                     let ty = &field.ty;
                     quote! {
-                        impl From<#ty> for #ident{
+                        impl #generics From<#ty> for #ident #generics{
                             fn from(variant: #ty)->Self{
                                 #ident::#var(variant)
                             }
